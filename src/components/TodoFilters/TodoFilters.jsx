1e7d9fbbd278edb16a3f7d86b755d3617cdf39f7
HaironHC <chavezhairon360@gmail.com>
2024-04-11 10:11:53 -0600

    Mejoramiento de fontend y backend de una aplicación web en react y nodejs

--- src/components/TodoFilters/TodoFilters.jsx
+++ src/components/TodoFilters/TodoFilters.jsx
@@ -12,7 +12,6 @@ const TodoFilters = () => {
         <FilterButton action={() => {}} active="All" filter="Active" />
         <FilterButton action={() => {}} active="All" filter="Completed" />
       </FilterButtonContainer>
-
       <button className="text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in-out">
         Clear Completed
       </button>
--- src/components/TodoInput/Todoinput.jsx
+++ src/components/TodoInput/Todoinput.jsx
@@ -1,4 +1,4 @@
-import { useState} from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const TodoInput = ({ addTodo }) => {
@@ -6,7 +6,6 @@ const TodoInput = ({ addTodo }) => {
   const [price, setPrice] = useState("");
 
   const handleAddTodo = (e) => {
-    
     //if(!title || !price) return;
 
     //if price is not null, press enter to focus on title input
@@ -18,7 +17,7 @@ const TodoInput = ({ addTodo }) => {
       document.getElementById("priceinput").focus();
     }
 
-    if (e.key.toLowerCase() === "enter" && title && price){
+    if (e.key.toLowerCase() === "enter" && title && price) {
       addTodo(title, price);
       setTitle(null);
       setPrice(null);
@@ -27,35 +26,55 @@ const TodoInput = ({ addTodo }) => {
   };
 
   return (
-
-    <div className="block sm:flex sm:w-full sm:px-5 ">
-    <div className="w-full">
-        <div className="
-        mt-6 w-full  relative 
-        sm:w-150px
-        ">
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+    <div
+      className="block 
+    sm:flex 
+    sm:w-full 
+    sm:justify-between
+  "
+    >
+      
+        <div
+          className="
+        mt-3  relative 
+        sm:w-[60%]
+        "
+        >
+          <div className=" absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
             <span className="border border-gray-500 border-solid p-3 rounded-full"></span>
           </div>
           <input
-          id="title"
+            id="title"
             type="text"
-            className="sm:flex-1 sm:w-full sm:flex-grow focus:shadow-lg font-Inter focus:shadow-blue-800 pl-12 w-full py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 ease-in-out"
+            className="sm:flex-1 
+            w-full
+            sm:w-full
+            sm:flex-grow 
+            focus:shadow-lg 
+            font-Inter focus:shadow-blue-800 pl-12 py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 ease-in-out"
             placeholder="cual es el siguiente"
             value={title ? title : ""}
             onChange={(e) => setTitle(e.target.value)}
             onKeyDown={(e) => handleAddTodo(e)}
           />
         </div>
-      </div>
+      
 
-      <div className="
-      mt-6 relative 
-      sm:ml-4 ">
+      <div
+        className="
+      mt-3 relative 
+      sm:ml-2 
+      sm:[30%]
+      "
+      
+      
+      >
         <input
           id="priceinput"
           type="text"
-          className="w-full focus:shadow-lg font-Inter focus:shadow-blue-800 pl-3 py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 ease-in-out"
+          className="sm- w-full focus:shadow-lg font-Inter focus:shadow-blue-800 pl-3 
+          py-4 bg-gray-700 rounded-xl outline-none transition-all duration-300 
+          ease-in-out"
           placeholder="precio"
           value={price ? price : ""}
           onChange={(e) => setPrice(e.target.value)}
@@ -68,4 +87,4 @@ const TodoInput = ({ addTodo }) => {
 
 export { TodoInput };
 
-TodoInput.propTypes = { addTodo: PropTypes.func.isRequired}
+TodoInput.propTypes = { addTodo: PropTypes.func.isRequired };
