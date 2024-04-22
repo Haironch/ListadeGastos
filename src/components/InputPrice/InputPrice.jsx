b93b8c34599aeba0e5e5b1f998fd51a2a9117fa6
HaironHC <chavezhairon360@gmail.com>
2024-04-22 12:07:02 -0600

    actualizar input price

--- src/components/InputPrice/InputPrice.jsx
+++ src/components/InputPrice/InputPrice.jsx
@@ -28,6 +28,7 @@ export default function InputPrice({setSalary}) {
           type="text"
           name="salary"
           id="salary"
+          inputMode="numeric"
           className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           placeholder="0.00"
           onChange={(e) => setSalaryInput(e.target.value)}
